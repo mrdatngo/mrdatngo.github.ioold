a7e2059244e866baa7bbac822c2b936aa93c6c10
mrdatngo <mrdatngo@gmail.com>
2020-05-12 07:45:58 +0700

    java2002 layout and 1/2 javascript day one

---
+++ JAVA2002/javascript-part/js/index.js
@@ -0,0 +1,29 @@
+// console.log("Hello")
+// alert("Hello world")
+var helloWorld = "Hello world! 2020"
+let helloWorld2 = "Hello world"
+const helloWorld3 = "Hello world"
+// const number;
+// number = 6;
+var number;
+number = 6;
+console.log(number)
+console.log(helloWorld, typeof(helloWorld))
+console.log(number, typeof(number))
+var x;
+var object = null
+console.log(x)
+console.log(object)
+
+// caculate timer
+var space = 120 + 100 - 60;
+var velocity = 20; 
+var timer = space/velocity;
+console.log(timer, "hours")
+console.log(9%5)
+var hello = "Hello" + " " + "world"
+console.log(hello)
+
+// mix
+var mix = 5 - Number("5");
+console.log("Mix: ", mix, typeof(mix))
