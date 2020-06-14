61aafb054791febaf846c14ad5f56f692ad7793d
mrdatngo <mrdatngo@gmail.com>
2020-06-14 20:33:04 +0700

    on done update caculator

--- JAVA2002/javascript-part/js/index8.js
+++ JAVA2002/javascript-part/js/index8.js
@@ -6,25 +6,41 @@ btnShow.addEventListener("click", () => {
     // alert(inputValue.value);
     pMessage.innerText = inputValue.value
 });
-// Caculate
+// Caculate ------ 
 var inputFirstNumber = document.getElementById("js-first-number");
 var inputSecondNumber = document.getElementById("js-second-number");
 var inputResult = document.getElementById("js-result");
-// var btnCaculate = document.getElementById("js-btn-caculate");
-// console.log(inputFirstNumber, inputSecondNumber, inputResult, btnCaculate);
+var operator = document.getElementById("js-operator");
+// var selectOperator = document.getElementById("js-operator");
 
 inputFirstNumber.oninput = caculate;
 inputSecondNumber.oninput = caculate;
+// selectOperator.onchange = caculate;
+document.getElementById("js-operator").onchange = caculate;
 
-// inputFirstNumber.addEventListener("input", function() {
-//     alert("Hihi")
-// })
-
-// btnCaculate.onclick = caculate;
+function getOperator() {
+    return operator.options[operator.selectedIndex].value;
+}
 
 function caculate() {
     let firstValue = Number(inputFirstNumber.value);
     let secondValue = Number(inputSecondNumber.value);
-    let total = firstValue + secondValue;
+    let operator = getOperator();
+    let total = 0;
+    switch (operator) {
+        case "+":
+            total = firstValue + secondValue;
+            break;
+        case "-":
+            total = firstValue - secondValue;
+            break;
+        case "*":
+            total = firstValue * secondValue;
+            break;
+        case "/":
+            total = firstValue / secondValue;
+            break;
+    }
+    // console.log("Total", String(total))
     inputResult.value = total;
 }
